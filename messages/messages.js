e8b6346d4e13a8fd024f0a46731decd66722493f
Bohdan <bohdan.martyniuk1488@gmail.com>
2019-04-15 14:54:37 -0400

    верстка card

--- messages/messages.js
+++ messages/messages.js
@@ -1,8 +1,10 @@
 import { Content } from './blocks/content/content';
 import { Letter } from './blocks/letter/letter';
+import { Card } from './blocks/card/card'
 /* eslint-disable */
 import _ from './messages.scss';
 /* eslint-enable */
 
 window.Content = Content;
 window.Letter = Letter;
+window.Card = Card;
--- messages/webpack.config.js
+++ messages/webpack.config.js
@@ -1,32 +1,32 @@
 module.exports = {
-    entry: './messages.js',
-    output: {
-        filename: 'messages.js'
-    },
-    devtool: 'eval-source-map',
-    module: {
+  entry: './messages.js',
+  output: {
+    filename: 'messages.js'
+  },
+  devtool: 'eval-source-map',
+  module: {
 
-        rules: [
-            {
-                test: /\.js$/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['babel-preset-env']
-                    }
-                }
-            },{
-                test: /\.pug$/,
-                use: 'pug-loader'
-            }, {
-                test: /\.scss$/,
-                use: [
-                    "style-loader",
-                    "css-loader",
-                    "sass-loader"
-                ]
-            }
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['babel-preset-env']
+          }
+        }
+      }, {
+        test: /\.pug$/,
+        use: 'pug-loader'
+      }, {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
         ]
+      }
+    ]
 
-    }
+  }
 };
