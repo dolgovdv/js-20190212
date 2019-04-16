a2c8b8ffb7f7b0c98af0e2c1687843deb3a423f3
yosherooshero <valeriya.korotkova@kaspersky.com>
2019-04-16 16:14:24 +0300

    Revert "сделана заготовка формы номера"
    This reverts commit 9839dfea9ad161d36f19439e2ef7f150f5e6bedc.

--- blocks/index.js
+++ blocks/index.js
@@ -1,10 +1,8 @@
 import { Textbox } from './textbox/textbox';
 import { Feedback } from './feedback/feedback';
-import { Phonenumber } from './phonenumber/phonenumber';
 /* eslint-disable */
 import _ from './index.scss';
 /* eslint-enable */
 
 window.Textbox = Textbox;
 window.Feedback = Feedback;
-window.Phonenumber = Phonenumber;
