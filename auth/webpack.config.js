1d1477e2db92ad54aa9a5521adbc60048b4c0dab
dolgovdv <dolgov.denis@gmail.com>
2019-04-16 18:08:24 +0300

    fix path package.json lint:auth "lint:auth": "eslint --fix --ext .js auth/", - чтоб вся папка auth попадала в линт при проверке и файлы с расширение .js Исключения размещены в для проверки lint размещены в файле .eslintignore
    Signed-off-by: dolgovdv <dolgov.denis@gmail.com>

--- auth/webpack.config.js
+++ auth/webpack.config.js
@@ -20,9 +20,9 @@ module.exports = {
     }, {
       test: /\.scss$/,
       use: [
-        "style-loader",
-        "css-loader",
-        "sass-loader"
+        'style-loader',
+        'css-loader',
+        'sass-loader'
       ]
     }]
 
