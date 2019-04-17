c10b405673388f63e53016d717e9ab67a17ac707
Bohdan <bohdan.martyniuk1488@gmail.com>
2019-04-16 20:26:42 -0400

    add path.resolve

--- messages/webpack.config.js
+++ messages/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-  entry: './messages.js',
+  entry: path.resolve('./messages.js'),
   output: {
     path: path.resolve('./dist'),
     filename: 'messages.js'
