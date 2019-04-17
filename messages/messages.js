b9416c9a291f8f574b0196bd5bfaa96b7bfb9cfc
Bohdan <bohdan.martyniuk1488@gmail.com>
2019-04-16 22:21:03 -0400

    added sidebar

--- messages/messages.js
+++ messages/messages.js
@@ -2,6 +2,8 @@ import { Content } from './blocks/content/content';
 import { Letter } from './blocks/letter/letter';
 import { Card } from './blocks/card/card';
 import { Search } from './blocks/search/search'
+
+import { Sidebar } from './views/sidebar/sidebar'
 /* eslint-disable */
 import _ from './messages.scss';
 /* eslint-enable */
@@ -10,3 +12,7 @@ window.Content = Content;
 window.Letter = Letter;
 window.Card = Card;
 window.Search = Search;
+window.Sidebar = Sidebar;
+
+window.addEventListener('DOMContentLoaded', () => {
+});
--- messages/views/sidebar/sidebar.js
+++ messages/views/sidebar/sidebar.js
@@ -1,12 +1,32 @@
 import template from './sidebar.pug';
 import { Block } from '../../../blocks/block';
+
+import { Card } from '../../blocks/card/card'
+import { Search } from '../../blocks/search/search';
+
 /* eslint-disable */
 import _ from './sidebar.scss';
 
 /* eslint-enable */
 
 export class Sidebar extends Block {
+  get bemName () {
+    return 'sidebar'
+  }
+
   template (data) {
     return template(data)
+  }
+
+  constructor () {
+    super();
+    this.search = new Search();
+    this.card = new Card();
+  }
+
+  render (el) {
+    super.render(el);
+    this.card.render(this.getElement('card'));
+    this.search.render(this.getElement('search'));
   }
 }
