17a0761d12bb2c524172f88f5f68eb7ee92a1fdc
Aleksandr Makhalov <amakhalov@gmail.com>
2019-04-18 11:41:01 +0300

    Добавил datebox

--- settings/blocks/index.js
+++ settings/blocks/index.js
@@ -3,6 +3,7 @@ import { Security } from './security/security';
 import { Button } from './button/button';
 import { Textareabox } from './textareabox/textareabox';
 import { PersonalData } from './personalData/personalData';
+import { Datebox } from './datebox/datebox';
 
 /* eslint-disable */
 import _ from './index.scss';
@@ -13,3 +14,4 @@ window.Security = Security;
 window.Button = Button;
 window.Textareabox = Textareabox;
 window.PersonalData = PersonalData;
+window.Datebox = Datebox;
