b6b8ebc950b7490aff66b18ac3a2511ad0575be6
Dmitriy Sorokin <evdiso@gmail.com>
2019-04-19 21:28:20 +0400

    update

--- blocks/index.js
+++ blocks/index.js
@@ -1,7 +1,7 @@
 import { Textbox } from './textbox/textbox';
 import { Feedback } from './feedback/feedback';
 import { Phonenumber } from './phonenumber/phonenumber';
-import { Gallerey } from './galarey/gallerey';
+import { Gallerey } from './gallerey/gallerey';
 
 /* eslint-disable */
 import _ from './index.scss';
